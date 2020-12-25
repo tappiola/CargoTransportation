952ac98b8635cca1ac5ada1a218091894bf7843f
MakMakAle <dranzerf@gmail.com>
2020-12-25 18:00:31 +0300

    Setup static folder for production mode

--- back/server.js
+++ back/server.js
@@ -1,11 +1,16 @@
 const express = require( 'express' );
 const cors = require( 'cors' );
+const path = require( 'path' );
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use( cors() );
 app.use( express.json() );
+
+if ( process.env.NODE_ENV === 'production' ) {
+   app.use( express.static( path.resolve( __dirname, 'front/build' ) ) );
+}
 
 //+ Error handler
 app.use( ( req, res, next ) => {
