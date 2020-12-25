b685f465a52f10ace17305640618eec377d521f9
MakMakAle <dranzerf@gmail.com>
2020-12-25 08:24:35 +0300

    Add error handler

--- back/server.js
+++ back/server.js
@@ -7,6 +7,19 @@ const PORT = process.env.PORT || 5000;
 app.use( cors() );
 app.use( express.json() );
 
+//+ Error handler
+app.use( ( req, res, next ) => {
+   const err = new Error( 'not found' );
+   err.status = 404;
+   next( err );
+} );
+
+app.use( ( err, req, res, next ) => {
+   const status = err.status || 500;
+   res.status( status ).json( { error : { message : err.message } } );
+} );
+//- Error handler
+
 app.get( '*', async ( req, res ) => {
    res.send( 'ok' );
 } );
