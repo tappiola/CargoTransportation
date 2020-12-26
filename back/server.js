5344d4ddfd681780d2a1d98326dce07faed3be7b
MakMakAle <dranzerf@gmail.com>
2020-12-26 12:18:03 +0300

    Configuration for production mode

--- back/server.js
+++ back/server.js
@@ -8,8 +8,14 @@ const PORT = process.env.PORT || 5000;
 app.use( cors() );
 app.use( express.json() );
 
+app.get( '/favicon.ico', ( req, res ) => res.status( 204 ) );
+
 if ( process.env.NODE_ENV === 'production' ) {
-   app.use( express.static( path.resolve( __dirname, 'front/build' ) ) );
+   app.use( '/', express.static( path.resolve( __dirname, 'front/build' ) ) );
+   
+   app.get( '*', ( req, res ) => {
+      res.sendFile( path.resolve( __dirname, 'front/build', 'index.html' ) );
+   } );
 }
 
 //+ Error handler
@@ -19,14 +25,14 @@ app.use( ( req, res, next ) => {
    next( err );
 } );
 
-app.use( ( err, req, res, next ) => {
+app.use( ( err, req, res ) => {
    const status = err.status || 500;
    res.status( status ).json( { error : { message : err.message } } );
 } );
 //- Error handler
 
 app.get( '*', async ( req, res ) => {
-   res.send( 'ok' );
+   res.json( { message : 'ok' } );
 } );
 
 app.listen( PORT, () => {
