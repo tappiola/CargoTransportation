46eb0c4872807864350ca6303066030252379e4c
VLADIMIR STOROZHENKO <storozhenko.vladimir.vasilhevich@gmail.com>
2020-12-27 02:02:56 +0300

    fix: commit validators.js

---
+++ front/src/components/NewCompanyForm/validators.js
@@ -0,0 +1,25 @@
+export const textValidator = (str) => typeof str === 'string';
+
+export const emailValidator = () => true; // already exist
+
+export const dateValidator = (date) => (
+  date instanceof Date
+  && date.getDay()
+  && date.getMonth()
+  && date.getFullYear()
+);
+
+export const adressValidator = ({ city, street, house /* apartment */ }) => (
+  textValidator(city) && textValidator(street) && parseInt(house, 10)
+);
+
+export const roleValidator = () => true;
+
+export const userDataValidator = (userData) => (
+  textValidator(userData.firstname)
+  && textValidator(userData.surname)
+  && emailValidator(userData.email)
+  && roleValidator(userData.role)
+  && adressValidator(userData.adress)
+  && dateValidator(userData.birthDate)
+);
