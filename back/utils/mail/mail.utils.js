c35c2fdfe3f8336a23aaa8a8c4881088fb1ca297
MakMakAle <dranzerf@gmail.com>
2021-01-04 10:06:27 +0300

    Change mail constants

--- back/utils/mail/mail.utils.js
+++ back/utils/mail/mail.utils.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const CLIENT_ID = '554603357178-hi8qfdsqro6nq16u93kc4dig17smve7l.apps.googleusercontent.com';
-const CLIENT_SECRET = 'FKMZmrZFhmKWo3Ou6rFW1uLc';
+const CLIENT_ID = '1012547823960-67illj7u6vchl2fu2a52s31qv42r16bf.apps.googleusercontent.com';
+const CLIENT_SECRET = 'BrfgMY8XYcOKGWa4HRVQPk0w';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04QHjwWs3LKEoCgYIARAAGAQSNwF-L9IrBY6OtvsIeAiag1r5FqyaHTk96jtvkRhRAtlzteaB8WdB7Gr6FbhOn7xXhwJOsGMAaJ4';
+const REFRESH_TOKEN = '1//04lm9BWSCO5CwCgYIARAAGAQSNwF-L9IraTDECZfVxXHTNrchHi0omRXWRPkbU7PGq3GSi_gh9oTH07LiMenDbo7O02mjCPphBC4';
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oAuth2Client.setCredentials({ refresh_token : REFRESH_TOKEN });
