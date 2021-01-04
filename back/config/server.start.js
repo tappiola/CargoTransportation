5fb1be04fcb25e4ce8bc6619dfaf5fe9f697b644
MakMakAle <dranzerf@gmail.com>
2021-01-04 14:39:27 +0300

    Change logger to Logger

--- back/config/server.start.js
+++ back/config/server.start.js
@@ -1,17 +1,17 @@
 const db = require('../database/db'),
-	PORT = process.env.PORT || 5000,
-	logger = require('../config/logger');
+  PORT = process.env.PORT || 5000,
+  Logger = require('../config/logger');
 
 module.exports = async ( app ) => {
-	try {
-		await db.authenticate();
-		logger.info('Database connected successfully');
-		
-		app.listen(PORT, () => {
-			logger.info(`Server has started on port ${PORT}`);
-		});
-	} catch (e) {
-		logger.error('Database connection FAILED');
-		process.exit(1);
-	}
+  try {
+    await db.authenticate();
+    Logger.info('Database connected successfully');
+    
+    app.listen(PORT, () => {
+      Logger.info(`Server has started on port ${PORT}`);
+    });
+  } catch (e) {
+    Logger.error('Database connection FAILED');
+    process.exit(1);
+  }
 };
