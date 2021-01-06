ef7fc1ecec62efd4bf0892152f09439fcfc547e6
Vladimir <storozhenko.vladimir.vasilhevich@gmail.com>
2021-01-06 18:38:32 +0300

    refactor: add Controller

--- front/src/components/UserForm/index.jsx
+++ front/src/components/UserForm/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import {
+  useForm,
+  useFormContext,
+  Controller,
+  FormProvider,
+} from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import TextField from '@material-ui/core/TextField';
@@ -22,94 +27,102 @@ import { dispatchSetUser } from 'redux/actions/users';
 import { useStyles } from './UserForm.styles';
 import { schema } from './userSchema';
 
+const BaseField = ({ name, errors, ...props }) => {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      as={TextField}
+      name={name}
+      control={control}
+      margin="normal"
+      error={errors && !!errors[name]}
+      helperText={errors && errors[name]?.message}
+      fullWidth
+      defaultValue=""
+      {...props}
+    />
+  );
+};
+
 function UserForm({ prevUserData, resolveSubmit }) {
   const birthDate = prevUserData?.birthDate || new Date();
 
   const classes = useStyles();
   const { id: _id } = useParams();
-  const { register, handleSubmit, errors } = useForm({
+  const methods = useForm({
     defaultValues: prevUserData,
     resolver: yupResolver(schema),
   });
-
-  const BaseField = ({ name, ...props }) => (
-    <TextField
-      name={name}
-      margin="normal"
-      inputRef={register}
-      error={!!errors[name]}
-      helperText={errors[name]?.message}
-      fullWidth
-      {...props}
-    />
-  );
-
+  const { register, handleSubmit, errors } = methods;
   return (
     <Container maxWidth="sm">
-      <form
-        noValidate
-        className={classes.form}
-        onSubmit={handleSubmit(({ roles: rolesAsObject, ...data }) => {
-          const rolesAsArray = Object.entries(rolesAsObject)
-            // eslint-disable-next-line no-unused-vars
-            .filter(([role, isChecked]) => isChecked)
-            .map(([role]) => role);
-          resolveSubmit({ ...data, roles: rolesAsArray, id: _id });
-        })}
-      >
-        <Grid container direction="column">
-          <BaseField name="firstName" label="Имя" autoComplete="given-name" />
-          <BaseField name="middleName" label="Отчество" autoComplete="additional-name" />
-          <BaseField name="lastName" label="Фамилия" autoComplete="family-name" />
-          <BaseField name="email" margin="normal" autoComplete="email" />
-          <BaseField name="password" label="Пароль" type="password" autoComplete="current-password" />
-          <Grid container spacing={1}>
-            <Grid item xs={6} sm={4}>
-              <BaseField name="city" label="Город" autoComplete="address-level2" />
+      <FormProvider {...methods}>
+        <form
+          noValidate
+          className={classes.form}
+          onSubmit={handleSubmit(({ roles: rolesAsObject, ...data }) => {
+            const rolesAsArray = Object.entries(rolesAsObject)
+              // eslint-disable-next-line no-unused-vars
+              .filter(([role, isChecked]) => isChecked)
+              .map(([role]) => role);
+            resolveSubmit({ ...data, roles: rolesAsArray, id: _id });
+          })}
+        >
+          <Grid container direction="column">
+            <BaseField name="firstName" label="Имя" autoComplete="given-name" errors={errors} />
+            <BaseField name="middleName" label="Отчество" autoComplete="additional-name" errors={errors} />
+            <BaseField name="lastName" label="Фамилия" autoComplete="family-name" errors={errors} />
+            <BaseField name="email" margin="normal" autoComplete="email" errors={errors} />
+            <BaseField name="password" label="Пароль" type="password" autoComplete="current-password" errors={errors} />
+            <Grid container spacing={1}>
+              <Grid item xs={6} sm={4}>
+                <BaseField name="city" label="Город" autoComplete="address-level2" errors={errors} />
+              </Grid>
+              <Grid item xs={6} sm={4}>
+                <BaseField name="street" label="Улица" errors={errors} />
+              </Grid>
+              <Grid item xs={6} sm={2}>
+                <BaseField name="house" label="Дом" autoComplete="address-level3" errors={errors} />
+              </Grid>
+              <Grid item xs={6} sm={2}>
+                <BaseField name="apartment" label="Квартира" autoComplete="address-level4" errors={errors} />
+              </Grid>
             </Grid>
-            <Grid item xs={6} sm={4}>
-              <BaseField name="street" label="Улица" />
-            </Grid>
-            <Grid item xs={6} sm={2}>
-              <BaseField name="house" label="Дом" autoComplete="address-level3" />
-            </Grid>
-            <Grid item xs={6} sm={2}>
-              <BaseField name="apartment" label="Квартира" autoComplete="address-level4" />
-            </Grid>
+            <FormControl error={!!errors.birthdate} margin="normal">
+              <InputLabel htmlFor="birthdate-input">Дата рождения *</InputLabel>
+              <Input
+                id="birthdate-input"
+                name="birthdate"
+                type="date"
+                inputRef={register}
+                defaultValue={birthDate.toISOString().substring(0, 10)}
+                errors={errors}
+              />
+              <FormHelperText>{errors?.birthdate?.message}</FormHelperText>
+            </FormControl>
+            <FormControl error={!!errors?.roles}>
+              <FormLabel component="legend">Роли:</FormLabel>
+              <FormGroup row>
+                {Object.entries(ROLE_NAMES)
+                  .filter(([roleName]) => roleName !== ROLES.GLOBAL_ADMIN)
+                  .map(([value, label]) => (
+                    <FormControlLabel
+                      key={value}
+                      control={
+                        <Checkbox inputRef={register} name={`roles.${value}`} />
+                      }
+                      label={label}
+                    />
+                  ))}
+              </FormGroup>
+              <FormHelperText>
+                {errors.roles && 'Выберите хотя бы одну роль'}
+              </FormHelperText>
+            </FormControl>
+            <SubmitButton className={classes.submit}>Готово</SubmitButton>
           </Grid>
-          <FormControl error={!!errors.birthdate} margin="normal">
-            <InputLabel htmlFor="birthdate-input">Дата рождения *</InputLabel>
-            <Input
-              id="birthdate-input"
-              name="birthdate"
-              type="date"
-              inputRef={register}
-              defaultValue={birthDate.toISOString().substring(0, 10)}
-            />
-            <FormHelperText>{errors?.birthdate?.message}</FormHelperText>
-          </FormControl>
-          <FormControl error={!!errors?.roles}>
-            <FormLabel component="legend">Роли:</FormLabel>
-            <FormGroup row>
-              {Object.entries(ROLE_NAMES)
-                .filter(([roleName]) => roleName !== ROLES.GLOBAL_ADMIN)
-                .map(([value, label]) => (
-                  <FormControlLabel
-                    key={value}
-                    control={
-                      <Checkbox inputRef={register} name={`roles.${value}`} />
-                    }
-                    label={label}
-                  />
-                ))}
-            </FormGroup>
-            <FormHelperText>
-              {errors.roles && 'Выберите хотя бы одну роль'}
-            </FormHelperText>
-          </FormControl>
-          <SubmitButton className={classes.submit}>Готово</SubmitButton>
-        </Grid>
-      </form>
+        </form>
+      </FormProvider>
     </Container>
   );
 }
