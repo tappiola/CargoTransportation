2070bbb64987d0868c903478b354e912ca7216b2
MakMakAle <dranzerf@gmail.com>
2021-01-18 11:05:56 +0300

    Replace logout before get by id

--- back/controllers/userСontroller.js
+++ back/controllers/userСontroller.js
@@ -1,19 +1,19 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const passport = require('passport');
 const Logger = require('../config/logger');
-const { createRandomPassword, passwordRegExp } = require('../utils/password.utils');
+const {createRandomPassword, passwordRegExp} = require('../utils/password.utils');
 const User = require('../models/User');
 const Role = require('../models/Role');
 const Company = require('../models/Company');
 const validate = require('../middlewares/validate');
-const { sendEmail, setMailOptions } = require('../utils/mail/mail.utils');
+const {sendEmail, setMailOptions} = require('../utils/mail/mail.utils');
 const registerTemplate = require('../utils/mail/tmpl/register');
 
 const router = Router();
 
 router.post('/register', validate.register, async (req, res, next) => {
-  const { email, ...userData } = req.body;
-  const user = await User.findOne({ where: { email } });
+  const {email, ...userData} = req.body;
+  const user = await User.findOne({where: {email}});
 
   if (user) {
     return res.sendError(401, 'Email already in use!');
@@ -37,7 +37,7 @@ router.post('/register', validate.register, async (req, res, next) => {
 
     sendEmail(mail).then((res) => console.log('Email sent...', res.messageId)).catch((err) => Logger.error(err.message));
 
-    res.status(200).json({ token });
+    res.status(200).json({token});
   } catch (e) {
     e.status = 400;
     next(e);
@@ -60,7 +60,7 @@ router.post('/login', async (req, res, next) => {
       }
 
       const token = user.generateJWT();
-      res.status(200).json({ token });
+      res.status(200).json({token});
     });
   })(req, res, next);
 });
@@ -71,14 +71,14 @@ router.get('/', async (req, res) => {
       {
         model: Role,
         attributes: [],
-        where: { role: 'admin' },
+        where: {role: 'admin'},
       },
       {
         model: Company,
         attributes: ['name', 'unn'],
       },
     ],
-    attributes:{
+    attributes: {
       exclude: ['password'],
     },
     order: [
@@ -89,13 +89,18 @@ router.get('/', async (req, res) => {
   res.status(200).json(users);
 });
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.status(204).json({});
+});
+
 router.get('/:id', async (req, res) => {
   const user = await User.get(req.params.id);
   res.status(200).json(user);
 });
 
 router.delete('/', async (req, res) => {
-  const { ids } = req.query;
+  const {ids} = req.query;
 
   await User.destroy({
     where: {
@@ -107,10 +112,10 @@ router.delete('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { password, ...userData } = req.body;
+  const {password, ...userData} = req.body;
   const user = await User.findByPk(req.params.id);
   const newPassword = passwordRegExp.test(password) && password;
-  
+
   if (!user) {
     return res.sendError(400, 'user not found');
   }
@@ -118,13 +123,8 @@ router.put('/:id', async (req, res) => {
     ...userData,
     password: newPassword || user.password,
   });
-  
-  res.status(200).json(user);
-});
 
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.status(204).json({});
+  res.status(200).json(user);
 });
 
 module.exports = router;
