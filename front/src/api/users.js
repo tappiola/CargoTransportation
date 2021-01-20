013af61a13bb5cc4753be74d98fae697065a73ef
Vladimir <storozhenko.vladimir.vasilhevich@gmail.com>
2021-01-20 15:58:32 +0300

    fix: unnecessary line

--- front/src/api/users.js
+++ front/src/api/users.js
@@ -8,4 +8,3 @@ export const setUser = async ({ data }) => fetchAPI(`${BASE_URI}/register`, data
 export const updateUser = async (data, id) => fetchAPI(`${BASE_URI}/${id}`, data, 'PUT');
 export const signIn = async (email, password) => fetchAPI(`${BASE_URI}/login`, { email, password }, 'POST');
 export const logoutUser = async () => fetchAPI(`${BASE_URI}/logout`, null).catch(() => true);
-
