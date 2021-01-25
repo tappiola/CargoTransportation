619095e9aa148d2c024cd7a5f3c0e065b03fc9d3
Vladimir <storozhenko.vladimir.vasilhevich@gmail.com>
2021-01-25 12:00:58 +0300

    feat: remove unnecessary

--- front/src/pages/Clients/Client.jsx
+++ front/src/pages/Clients/Client.jsx
@@ -1,45 +1,8 @@
-/* eslint-disable implicit-arrow-linebreak */
-import React, { useEffect, useState } from 'react';
-
-import { List, ListItem, TextField } from '@material-ui/core';
-
-import { useElastic } from 'api';
-
-const intersection = (first, ...other) =>
-  first.filter(({ id }) =>
-    other.every((results) =>
-      results.some(({ id: someId }) => id === someId)));
+import { useParams } from 'react-router-dom';
 
 function Client() {
-  const { options: fromFirstName, onChange: onFirstNameChange } = useElastic('users', 'firstName');
-  const { options: fromLastName, onChange: onLastNameChange } = useElastic('users', 'lastName');
-  const [results, setResults] = useState([]);
-  const fieldsDataArr = [fromLastName, fromFirstName];
-
-  useEffect(() => {
-    const [first, ...other] = fieldsDataArr.filter((i) => i);
-    setResults(other.length ? intersection(first, ...other) : first);
-  }, [...fieldsDataArr]);
-
-  return (
-    <>
-      <TextField onChange={onFirstNameChange} label="имя" />
-      <TextField onChange={onLastNameChange} label="фамилия" />
-      <List>
-        {results
-          ? results.map(({ firstName, lastName, id }) => (
-            <ListItem key={id}>
-              <p>
-                {firstName}
-                &nbsp;
-                {lastName}
-              </p>
-            </ListItem>
-          ))
-          : 'Не найдено'}
-      </List>
-    </>
-  );
+  const { id } = useParams();
+  return `client ${id}`;
 }
 
 export default Client;
