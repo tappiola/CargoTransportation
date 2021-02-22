072d49e5f9d625a8700fbaeccfe2d3b7cc3cbbb5
MakMakAle <dranzerf@gmail.com>
2021-02-22 11:57:42 +0300

    Fix: page refresh error on heroku

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const routes = require('./back/routes');
 const serverConfig = require('./back/config/server.config');
 const errorHandler = require('./back/middlewares/errorHandler');
 const serverStart = require('./back/config/server.start');
-const expressWs = require('express-ws')(app);
+require('express-ws')(app);
 
 serverConfig(app);
 routes(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('front/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'front/build', 'index.html'));
+  app.use(express.static(path.join(__dirname, 'front/build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front/build', 'index.html'));
   });
 }
 
