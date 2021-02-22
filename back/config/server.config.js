eb15377544a0602b1a90c558f3c06cb2c3b8e735
MakMakAle <dranzerf@gmail.com>
2021-02-22 17:21:45 +0300

    Add favicon

--- back/config/server.config.js
+++ back/config/server.config.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
+const favicon = require('express-favicon');
 const sessionConfig = require('./session.config');
 
 module.exports = (app) => {
@@ -10,6 +12,6 @@ module.exports = (app) => {
   app.use(cors());
   app.use(express.json({ limit: '5mb' }));
   app.use(express.urlencoded({ extended: true }));
-  app.get('/favicon.ico', (req, res) => res.status(204));
+  app.use(favicon(path.jsoin(__dirname, '../../front/public/favicon.ico')));
   sessionConfig(app);
 };
